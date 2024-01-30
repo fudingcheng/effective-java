f4895421c0b6e89ddff4859ee6ba43be638b9b3d
fudingcheng <fudingcheng0314@gmail.com>
2024-01-30 12:31:59 +0800

    effectivejava

---
+++ src/effectivejava/chapter2/item3/enumtype/Elvis.java
@@ -0,0 +1,19 @@
+package effectivejava.chapter2.item3.enumtype;
+
+/**
+ * 使用枚举是创建单例最好的方式
+ */
+// Enum singleton - the preferred approach (Page 18)
+public enum Elvis {
+    INSTANCE;
+
+    public void leaveTheBuilding() {
+        System.out.println("Whoa baby, I'm outta here!");
+    }
+
+    // This code would normally appear outside the class!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.INSTANCE;
+        elvis.leaveTheBuilding();
+    }
+}
---
+++ src/effectivejava/chapter2/item3/staticfactory/Elvis.java
@@ -0,0 +1,20 @@
+package effectivejava.chapter2.item3.staticfactory;
+
+import java.io.Serializable;
+
+// Singleton with static factory (Page 17)
+public class Elvis implements Serializable {
+    private static final Elvis INSTANCE = new Elvis();
+    private Elvis() { }
+    public static Elvis getInstance() { return INSTANCE; }
+
+    public void leaveTheBuilding() {
+        System.out.println("Whoa baby, I'm outta here!");
+    }
+
+    // This code would normally appear outside the class!
+    public static void main(String[] args) {
+        Elvis elvis = Elvis.getInstance();
+        elvis.leaveTheBuilding();
+    }
+}
